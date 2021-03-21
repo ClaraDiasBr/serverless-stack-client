c4869070bf3e0134ffcc6b12f4385da67033755c
Clara Dias <claradprado@gmail.com>
2021-03-20 21:51:31 -0300

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 const config = {
+    STRIPE_KEY: "pk_test_51IWqgsCuu7TBiVaXWuB0Jfg5w7iprrXuudEgjaLBX4vYo6ARdMNx1KZwIwlS0LBF08epd7T3UPJYgUAWaGXTYpzi00X9BPNLCp",
+    MAX_ATTACHMENT_SIZE: 5000000,
+    
     s3: {
       REGION: "us-east-1",
       BUCKET: "note-app-upload96",
@@ -13,6 +16,6 @@ const config = {
       APP_CLIENT_ID: "2hek7d2g5h00d69ncrbpsigdb",
       IDENTITY_POOL_ID: "us-east-1:4270d8a5-38c8-4dba-8ea2-f5a3c35f4e10",
     },
-  };
+};
   
-  export default config;
+export default config;
